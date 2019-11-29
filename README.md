4e48fbbe46e966bcae475978c0dbf50a81b0326a
hwk <loool@lool.om>
2019-11-29 02:07:03 +0200

    screenshots ui

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # filemanager-media
 Media features for filemanager plugin which include:
-- video player for common video formats: mp4|avi|divx|mkv (browser dependent)
-- video screenshots functionality
+- audio/video player for common media formats: mp3|mp4|avi|divx|mkv (browser dependent)
+- image viewer
+- video screenshots functionality: multiple screenshots, single mosaic
+
+Screenshots format - default  is using the screenshots plugin if available
+ 
+Moisaic format needs to be fixed. 
 
 ***Setup:***
   - edit the url in conf.php where view.php is accessible `$streampath = '/plugins/filemanager-media/view.php';`
--- init.js
+++ init.js
@@ -5,7 +5,6 @@ plugin.loadLang();
 
 (function flmMedia(window) {
 
-
 	var media = {
 		stp:  'plugins/mediastream/view.php'
 	};
@@ -18,15 +17,6 @@ plugin.loadLang();
 					media.stop();
 				}
 			});
-/*
-		theWebUI.fManager.action.request('action=sess',
-			function (data) {
-				if(theWebUI.fManager.isErr(data.errcode)) {log('Play failed'); return false;}
-				theWebUI.fManager.makeVisbile('VPLAY_diag');
-				try {
-					theWebUI.VPLAY.player.Open(theWebUI.VPLAY.stp+'?ses='+encodeURIComponent(data.sess)+'&action=view&dir='+encodeURIComponent(theWebUI.fManager.curpath)+'&target='+encodeURIComponent(target));
-				} catch(err) { }
-			});*/
 	};
 
 	media.getVideoPlayer = function()
@@ -65,24 +55,14 @@ plugin.loadLang();
 
 		this.actStart(diag);
 
-		this.action.request('action=scrn&target=' + encodeURIComponent(video) + '&to=' + encodeURIComponent(screen_file));
-
-
-		var actioncall = {
-			method: 'fileScreenSheet',
-			target: video,
-			to: screen_file
-		};
-
-
-		this.action.postRequest({action: flm.utils.json_encode(actioncall)});
-
 
 	};
 
 	media.setDialogs = function(flmDialogs) {
 
-		flm.views.namespaces['flm-media'] = plugin.path + 'views/';
+		var viewsPath = plugin.path + 'views/';
+
+		flm.views.namespaces['flm-media'] = viewsPath;
 
 		flmDialogs.forms['flm-media-player'] = {
 				options: {
@@ -90,7 +70,17 @@ plugin.loadLang();
 					views: "flm-media"
 				},
 				modal: false,
-				template: plugin.path + "views/dialog-media-player"
+				template: viewsPath + "dialog-media-player"
+		};
+
+		flmDialogs.forms['media-screenshots'] = {
+			options: {
+				public_endpoint: plugin.config.public_endpoint,
+				views: "flm-media"
+			},
+			modal: true,
+			pathbrowse: true,
+			template: viewsPath + "dialog-screenshots"
 		};
 	};
 
@@ -100,28 +90,48 @@ plugin.loadLang();
 
 		if(plugin.enabled) {
 
-			var el = theContextMenu.get(theUILang.fOpen);
-			if(el && flm.utils.getExt(path).match(/^(mp[34]|avi|divx|mkv|png|jpeg|gif)$/i)) {
+			var ext = flm.utils.getExt(path);
 
-				menu.add(el,[CMENU_SEP]);
-				menu.add(el,[theUILang.fView, function() {media.play(path);}]);
-				menu.add(el,[CMENU_SEP]);
+			if(ext.match(/^(mp[34]|avi|divx|mkv|png|jpeg|gif)$/i)) {
 
+				var el = theContextMenu.get(theUILang.fOpen);
+				if(el)
+				{
+					menu.add(el,[CMENU_SEP]);
+					menu.add(el,[theUILang.fView, function() {media.play(path);}]);
+					menu.add(el,[CMENU_SEP]);
+				}
 
-				var sub = theContextMenu.get(theUILang.fcreate).children().last();
-				menu.add(sub,[theUILang.fcScreens, (
-					thePlugins.isInstalled('screenshots')
-					&& !pathIsDir
-					&& flm.utils.getExt(path).match(new RegExp("^(" + thePlugins.get('screenshots').extensions.join('|') + ")$", "i"))
-				) ? function () {
-						console.log('need screenshots?');
+				if(ext.match(/^(mp4|avi|divx|mkv)$/i)) {
+					var sub = theContextMenu.get(theUILang.fcreate).children().last();
+					if(sub)
+					{
+						menu.add(sub,[theUILang['flm_popup_media-screenshots'], /*(
+							thePlugins.isInstalled('screenshots')
+							&& !pathIsDir
+							&& flm.utils.getExt(path).match(new RegExp("^(" + thePlugins.get('screenshots').extensions.join('|') + ")$", "i"))
+						) */
+						true
+							? function () {
+								console.log('need screenshots?');
+								flm.ui.getDialogs().showDialog('media-screenshots');
+							}
+							: null]);
 					}
-					: null]);
-
-
+				}
 			}
+
 		}
 
+	};
+
+	window.flm.api.createScreensheet = function(source, destination, options) {
+		return window.flm.api.post({
+			method: 'fileScreenSheet',
+			options: options,
+			target: source,
+			to: destination
+		});
 	};
 
 	//onSetEntryMenu
@@ -144,16 +154,6 @@ plugin.loadLang();
 
 
 plugin.onLangLoaded = function() {
-
-	/*
-        injectScript('plugins/mediastream/settings.js.php');
-    */
-
-/*
-
-	theWebUI.VPLAY.player =  document.getElementById('np_plugin');
-	theDialogManager.setHandler('VPLAY_diag','afterHide', "theWebUI.VPLAY.stop()");*/
-
 	plugin.markLoaded();
 
 };
