6433cb8bcf7861da678c9d02fd0e29733f3a1429
nelu <unzel@unzel.com>
2022-01-21 23:31:15 +0200

    switched to psr-4 class autoloader

--- boot.php
+++ boot.php
@@ -1,23 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $flmPluginDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '../filemanager';
 
-
-require_once($flmPluginDir . '/src/BaseController.php');
-require_once($flmPluginDir . '/src/FileManager.php');
-require_once($flmPluginDir . '/src/RemoteShell.php');
-require_once($flmPluginDir . '/src/Filesystem.php');
-require_once($flmPluginDir . '/src/TaskController.php');
-require_once($flmPluginDir . '/src/FsUtils.php');
-require_once($flmPluginDir . '/../_task/task.php');
-require_once($flmPluginDir . '/src/Helper.php');
-
-$pluginDir = dirname(__FILE__) . DIRECTORY_SEPARATOR;
-require_once($pluginDir . 'src/Screenshots.php');
-require_once($pluginDir . 'src/FileManagerMedia.php');
-require_once($pluginDir . "/../../php/util.php");
+require_once($flmPluginDir . '/boot.php');
+\Flm\Helper::registerAutoload(dirname(__FILE__) . '/src', 'Flm\\Media');
+require_once (__DIR__ . '/../_task/task.php');
 
