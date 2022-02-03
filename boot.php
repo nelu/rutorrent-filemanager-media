00b64ff8aeb7314deed193a81b108b1d92ab8988
nelu <unzel@unzel.com>
2022-02-03 02:03:17 +0200

    autoloader updates

--- boot.php
+++ boot.php
@@ -4,5 +4,3 @@ $flmPluginDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . '../filemanager';
 
 require_once($flmPluginDir . '/boot.php');
 \Flm\Helper::registerAutoload(dirname(__FILE__) . '/src', 'Flm\\Media');
-require_once (__DIR__ . '/../_task/task.php');
-
